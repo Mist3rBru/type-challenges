185952b94cb26ce45f535081851fbe20f1f13209
Bruno Rocha <brunorocha2674@gmail.com>
2023-02-03 17:18:37 -0300

    add inludes resolution

--- playground/solved/easy/00898-easy-includes.ts
+++ playground/solved/easy/00898-easy-includes.ts
@@ -18,7 +18,19 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Includes<T extends readonly any[], U> = any
+type MyEqual<X, Y> =
+    (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y
+      ? 1
+      : 2
+      ? true
+      : false
+
+type Includes<T extends readonly any[], U> =
+    T extends [infer First, ...infer Rest]
+      ? MyEqual<First, U> extends true
+        ? true
+        : Includes<Rest, U>
+      : false
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
