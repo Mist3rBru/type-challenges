e020862b45b126e8e7a2e9441509611d629886ed
Bruno Rocha <brunorocha2674@gmail.com>
2023-02-06 13:16:54 -0300

    add join resolution

--- playground/solved/medium/05310-medium-join.ts
+++ playground/solved/medium/05310-medium-join.ts
@@ -19,7 +19,12 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Join<T, U> = any
+type Join<T, U extends string | number> =
+  T extends [infer L extends string, ...infer R]
+    ? R['length'] extends 0
+      ? L
+      : `${L}${U}${Join<R, U>}`
+    : ''
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
