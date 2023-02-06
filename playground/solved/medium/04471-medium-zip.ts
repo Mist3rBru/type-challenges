db3012e82551b4fd1ce972c75c19f34ba57fe788
Bruno Rocha <brunorocha2674@gmail.com>
2023-02-06 17:03:54 -0300

    add zip array resolution

--- playground/solved/medium/04471-medium-zip.ts
+++ playground/solved/medium/04471-medium-zip.ts
@@ -15,7 +15,12 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Zip<T, U> = any
+type Zip<T, U> =
+  T extends [infer L1, ...infer R1]
+    ? U extends [infer L2, ...infer R2]
+      ? [[L1, L2], ...Zip<R1, R2>]
+      : []
+    : []
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
