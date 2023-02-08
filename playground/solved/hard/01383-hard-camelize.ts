a2f0ecbf115dc2ffed7f0531300029385aca9d55
Bruno Rocha <brunorocha2674@gmail.com>
2023-02-08 10:32:01 -0300

    chore(hard): add camelize resolution

--- playground/solved/hard/01383-hard-camelize.ts
+++ playground/solved/hard/01383-hard-camelize.ts
@@ -27,24 +27,44 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Camelize<T> = any
+type CamelCase<S extends string, P extends string = ''> =
+  S extends `${infer F}${infer R}`
+    ? F extends '_'
+      ? P extends ''
+        ? `${F}${CamelCase<R, F>}`
+        : CamelCase<R, F>
+      : P extends '_'
+        ? `${Uppercase<F>}${CamelCase<R, F>}`
+        : `${Lowercase<F>}${CamelCase<R, F>}`
+    : ''
+
+type Camelize<T> =
+  T extends any[]
+    ? { [K in keyof T]: Camelize<T[K]> }
+    : T extends Record<string, any>
+      ? { [K in keyof T as K extends string ? CamelCase<K> : K]: Camelize<T[K]> }
+      : T
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
 
+type Result = Camelize<{
+  some_prop: string
+  0: number
+  prop: { another_prop: string }
+  array: [
+    { snake_case: string },
+    { another_element: { yet_another_prop: string } },
+    { yet_another_element: string },
+  ]
+}>
+
 type cases = [
   Expect<Equal<
-    Camelize<{
-      some_prop: string
-      prop: { another_prop: string }
-      array: [
-        { snake_case: string },
-        { another_element: { yet_another_prop: string } },
-        { yet_another_element: string },
-      ]
-    }>,
+    Result,
     {
       someProp: string
+      0: number
       prop: { anotherProp: string }
       array: [
         { snakeCase: string },
